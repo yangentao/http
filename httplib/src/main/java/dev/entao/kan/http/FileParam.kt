bade44bf81bd73ca1af1970dfab0c8d9038aa86b
yangentao <entaoyang@163.com>
2019-08-22 05:57:56 +0800

    4.0.2

--- httplib/src/main/java/dev/entao/kan/http/FileParam.kt
+++ httplib/src/main/java/dev/entao/kan/http/FileParam.kt
@@ -1,43 +1,54 @@
 package dev.entao.kan.http
 
 import android.net.Uri
+import android.webkit.MimeTypeMap
 import dev.entao.kan.base.Progress
+import java.io.File
 
 /**
  * Created by entaoyang@163.com on 2016-11-12.
  */
 
 //file, key, filename, mime都不能是空
-class FileParam(val key: String,
-                val file: Uri,
-                var filename: String = file.lastPathSegment ?: "",
-                var mime: String = "application/octet-stream"
+class FileParam(
+    val key: String,
+    val file: Uri,
+    var filename: String = file.lastPathSegment ?: "",
+    var mime: String = "application/octet-stream"
 ) {
 
 
-	var progress: Progress? = null
+    var progress: Progress? = null
 
 
-	fun mime(mime: String?): FileParam {
-		if (mime != null) {
-			this.mime = mime
-		}
-		return this
-	}
+    fun mime(mime: String?): FileParam {
+        if (mime != null) {
+            this.mime = mime
+        }
+        return this
+    }
 
-	fun fileName(filename: String?): FileParam {
-		if (filename != null) {
-			this.filename = filename
-		}
-		return this
-	}
+    fun fileName(filename: String?): FileParam {
+        if (filename != null) {
+            this.filename = filename
+        }
+        return this
+    }
 
-	fun progress(progress: Progress?): FileParam {
-		this.progress = progress
-		return this
-	}
+    fun progress(progress: Progress?): FileParam {
+        this.progress = progress
+        return this
+    }
 
-	override fun toString(): String {
-		return "key=$key, filename=$filename, mime=$mime, file=${file.toString()}"
-	}
+    override fun toString(): String {
+        return "key=$key, filename=$filename, mime=$mime, file=${file.toString()}"
+    }
+}
+
+fun MimeOfFile(file: File): String {
+    val ext = file.extension
+    if (ext != file.name && ext.isNotEmpty()) {
+        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext) ?: "application/octet-stream"
+    }
+    return "application/octet-stream"
 }
